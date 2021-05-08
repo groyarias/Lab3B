63743c02a3ab458ecf71f21613ab567ec1be17e3
groyarias <german.arias.sandoval@est.una.ac.cr>
2021-05-08 10:37:33 -0600

    Primer commit y vista Login

---
+++ app/src/main/java/com/example/lab03b/RegistroActivity.kt
@@ -0,0 +1,12 @@
+package com.example.plantillalogin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.lab03b.R
+
+class RegistroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registro)
+    }
+}
