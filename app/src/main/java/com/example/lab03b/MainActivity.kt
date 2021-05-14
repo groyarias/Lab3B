0c4f3a42fc8d7d6a4e16b4323445fac3b1ae0209
Koke1203 <jorge.jordi6@gmail.com>
2021-05-14 09:07:22 -0600

    Nav Drawer, Fragments y Formularios

---
+++ app/src/main/java/com/example/lab03b/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.example.lab03b
+
+import android.app.Activity
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.navigation.NavigationView
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        val toogle = ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open,R.string.close)
+        toogle.isDrawerIndicatorEnabled=true
+        drawerLayout.addDrawerListener(toogle)
+        toogle.syncState()
+
+        //Funcion del nav menu
+
+        nav_menu.setNavigationItemSelectedListener(this)
+
+        //Abrir fragments
+        setToolbartitle("Home")
+        changeFragment(Home())
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        drawerLayout.closeDrawer(GravityCompat.START)
+
+        when(item.itemId){
+            R.id.home->{
+                setToolbartitle("Home")
+                changeFragment(Home())
+
+            }
+            R.id.job->{
+                setToolbartitle("Job Application")
+                changeFragment(JobApplication())
+            }
+        }
+        return true
+    }
+
+    //Cambiar nombre del toolbar
+    fun setToolbartitle(title:String){
+
+        supportActionBar?.title=title
+    }
+    
+    fun changeFragment(frag:Fragment){
+        val fragment=supportFragmentManager.beginTransaction()
+        fragment.replace(R.id.fragment_container,frag).commit()
+    }
+
+
+}
